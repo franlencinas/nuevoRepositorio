1bde89d5da121e497dcca20a3a6a041e0434878c
franlencinas <franlencinasabdo@gmail.com>
2020-06-05 16:28:13 -0300

    Primer Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Musical Hendrix es una tienda de instrumentos musicales con ya más de 15
+    años de experiencia. Tenemos el conocimiento y la capacidad como para
+    informarte acerca de las mejores elecciones para tu compra musical.
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+.home {
+  margin: 5%;
+  font-size: 30px;
 }
-</script>
+</style>
